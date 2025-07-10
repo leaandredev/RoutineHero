9d5e03eece5c1bf1f2eb68986f1e6326acb9c006
Lea ANDRE <aleaandre@gmail.com>
2025-07-10 20:52:26 +0200

    [AF-626] feat add profile : display create profile button

--- routine-hero/src/app/features/parent/parent-dashboard/parent-dashboard.component.ts
+++ routine-hero/src/app/features/parent/parent-dashboard/parent-dashboard.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-parent-dashboard',
-  imports: [RouterLink],
+  imports: [RouterLink, MatButtonModule, MatIconModule, MatDividerModule],
   templateUrl: './parent-dashboard.component.html',
   styleUrl: './parent-dashboard.component.scss',
 })

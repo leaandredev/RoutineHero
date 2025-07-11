84824b56cea7ca2e1f4c0a920804191a6ab24421
Lea ANDRE <aleaandre@gmail.com>
2025-07-11 16:05:08 +0200

    [AF-631] create profile : profile picture select

--- routine-hero/src/app/features/parent/profil/profil-form/profil-form.component.ts
+++ routine-hero/src/app/features/parent/profil/profil-form/profil-form.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { ImageSelectComponent } from '../../../../core/components/image-select/image-select.component';
 
 @Component({
   selector: 'app-profil-form',
-  imports: [],
+  imports: [ImageSelectComponent],
   templateUrl: './profil-form.component.html',
-  styleUrl: './profil-form.component.scss'
+  styleUrl: './profil-form.component.scss',
 })
-export class ProfilFormComponent {
-
-}
+export class ProfilFormComponent {}

5c816aa801c87c41c7d6796c82da05aade455fcd
Lea ANDRE <aleaandre@gmail.com>
2025-07-14 09:14:38 +0200

    [AF-630] feat create profile : display a default picture for ImageSelectComponent and use ControlValueAccessor to make the component a field for the form

--- routine-hero/src/app/core/components/image-select/image-select.component.ts
+++ routine-hero/src/app/core/components/image-select/image-select.component.ts
@@ -1,5 +1,16 @@
-import { Component, Output } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {
+  Component,
+  EventEmitter,
+  forwardRef,
+  OnInit,
+  Output,
+} from '@angular/core';
+import {
+  ControlValueAccessor,
+  FormsModule,
+  NG_VALUE_ACCESSOR,
+  ReactiveFormsModule,
+} from '@angular/forms';
 import { MatSelectModule } from '@angular/material/select';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { CommonModule } from '@angular/common';
@@ -13,12 +24,21 @@ import { CommonModule } from '@angular/common';
     FormsModule,
     ReactiveFormsModule,
   ],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => ImageSelectComponent),
+      multi: true,
+    },
+  ],
   templateUrl: './image-select.component.html',
   styleUrl: './image-select.component.scss',
 })
-export class ImageSelectComponent {
-  selectedPicture: string | null = '/profile_icons/knight.png';
-  profilePictures = [
+export class ImageSelectComponent implements ControlValueAccessor {
+  private defaultPicture = '/profile_icons/non_selected_picture.png';
+  public selectedPicture: string = this.defaultPicture;
+
+  public profilePictures = [
     { label: 'Chevalier', path: '/profile_icons/knight.png' },
     { label: 'Cyclope', path: '/profile_icons/cyclops_5064863.png' },
     { label: 'Princesse', path: '/profile_icons/princess_5101538.png' },
@@ -28,4 +48,29 @@ export class ImageSelectComponent {
     { label: 'Viking', path: '/profile_icons/viking_5101673.png' },
     { label: 'Yéti', path: '/profile_icons/yeti_1149401.png' },
   ];
+
+  onChange: (value: string) => void = () => {};
+  onTouched: () => void = () => {};
+
+  writeValue(value: string): void {
+    this.selectedPicture = value;
+  }
+
+  registerOnChange(fn: (value: string) => void): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: () => void): void {
+    this.onTouched = fn;
+  }
+
+  onPictureSelected(picturePath: string) {
+    this.selectedPicture = picturePath;
+    this.onChange(picturePath);
+    this.onTouched;
+  }
+
+  get pictureDisplay() {
+    return this.selectedPicture || this.defaultPicture;
+  }
 }
--- routine-hero/src/app/features/parent/profil/profil-form/profil-form.component.ts
+++ routine-hero/src/app/features/parent/profil/profil-form/profil-form.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ImageSelectComponent } from '../../../../core/components/image-select/image-select.component';
+import { CommonModule } from '@angular/common';
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-profil-form',
-  imports: [ImageSelectComponent],
+  imports: [
+    ImageSelectComponent,
+    CommonModule,
+    FormsModule,
+    MatIconModule,
+    MatInputModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+  ],
   templateUrl: './profil-form.component.html',
   styleUrl: './profil-form.component.scss',
 })
-export class ProfilFormComponent {}
+export class ProfilFormComponent implements OnInit {
+  public form: FormGroup | undefined;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.form = this.fb.group({
+      profilePicture: ['', Validators.required],
+      name: ['', Validators.required],
+    });
+  }
+
+  public back() {
+    window.history.back();
+  }
+}

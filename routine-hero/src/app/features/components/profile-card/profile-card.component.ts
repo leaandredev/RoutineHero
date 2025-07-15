5c7896a15f3f3ec87dc1a9588ca7017994725e30
Lea ANDRE <aleaandre@gmail.com>
2025-07-15 15:38:44 +0200

    [AF-635] feat display list profile : handle parent/children status in the app

--- routine-hero/src/app/features/components/profile-card/profile-card.component.ts
+++ routine-hero/src/app/features/components/profile-card/profile-card.component.ts
@@ -1,8 +1,10 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { Profile } from '../../../core/interfaces/profile.interface';
+import { ContextService } from '../../../core/services/context.service';
+import { AppContext } from '../../../core/models/app-context.enum';
 
 @Component({
   selector: 'app-profile-card',
@@ -10,6 +12,13 @@ import { Profile } from '../../../core/interfaces/profile.interface';
   templateUrl: './profile-card.component.html',
   styleUrl: './profile-card.component.scss',
 })
-export class ProfileCardComponent {
+export class ProfileCardComponent implements OnInit {
   @Input() profile: Profile | null = null;
+  public canModify: boolean = false;
+
+  constructor(private contextService: ContextService) {}
+
+  ngOnInit(): void {
+    this.canModify = this.contextService.getCurrentValue() == AppContext.Parent;
+  }
 }
--- routine-hero/src/app/features/parent/parent-dashboard/parent-dashboard.component.ts
+++ routine-hero/src/app/features/parent/parent-dashboard/parent-dashboard.component.ts
@@ -1,13 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatButtonModule } from '@angular/material/button';
+import { ListeProfilesComponent } from '../../components/liste-profiles/liste-profiles.component';
+import { ContextService } from '../../../core/services/context.service';
+import { AppContext } from '../../../core/models/app-context.enum';
 
 @Component({
   selector: 'app-parent-dashboard',
-  imports: [RouterLink, MatButtonModule, MatIconModule, MatDividerModule],
+  imports: [
+    RouterLink,
+    MatButtonModule,
+    MatIconModule,
+    MatDividerModule,
+    ListeProfilesComponent,
+  ],
   templateUrl: './parent-dashboard.component.html',
   styleUrl: './parent-dashboard.component.scss',
 })
-export class ParentDashboardComponent {}
+export class ParentDashboardComponent implements OnInit {
+  constructor(private contextService: ContextService) {}
+
+  ngOnInit(): void {
+    this.contextService.setContext(AppContext.Parent);
+  }
+}
